921053aa126f92de68f72a8fae18ba938e8ab734
Domi <vataidomonkos@gmail.com>
2022-10-09 16:55:12 +0200

    format

--- szorgalmi 2/combobox/index.js
+++ szorgalmi 2/combobox/index.js
@@ -1,6 +1,5 @@
 import { wordOptions } from './words.js';
 
-//<div class="word-option">foo</div>
 const inputRef = document.querySelector('#input');
 const popupRef = document.querySelector('#popup');
 
