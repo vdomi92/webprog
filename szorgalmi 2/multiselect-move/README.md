260e9e6e955c8700eab13e1eb57c84163bd274a3
Domi <vataidomonkos@gmail.com>
2022-10-09 14:17:12 +0200

    szorgalmi multiselect-move

---
+++ szorgalmi 2/multiselect-move/README.md
@@ -0,0 +1,3 @@
+A feladatokhoz tartozó kiinduló HTML kódokat a https://github.com/vimtaai/elte/tree/master/2022-23-1/wp-e1/gyak-03 oldalon találod.
+
+1. multiselect-move: Adott az oldalon két lista űrlapelem (select), ezek között két gomb: az egyikre kattintva a bal oldali listában kiválasztott elem átkerül a jobb oldali listába, a másikra kattintva ugyanez történik jobbról balra. A listákban többszörös választás is lehetséges, úgy készítsd el a megoldásodat, hogy ha több elem is ki van választva, akkor mindegyik átkerüljön a másik oldalra.
---
+++ szorgalmi 2/multiselect-move/index.js
@@ -0,0 +1,63 @@
+import { wordOptions } from './words.js';
+
+const leftSelect = document.querySelector('#left-select');
+const rightSelect = document.querySelector('#right-select');
+const selectParent = document.querySelector('#multiselect-move');
+
+//Function to generate select options from indices array and data array
+const createSelectOptionsByIndex = (indices) => {
+  return indices.map((index) => {
+    return `<option value="${index}"> ${wordOptions[index]} </option>`;
+  });
+};
+
+//Filling up options with initial data
+const initializeSelectOptions = () => {
+  let selectOptions = wordOptions.map((word, i) => {
+    return `<option value="${i}"> ${word} </option>`;
+  });
+  leftSelect.innerHTML = selectOptions.join('\n');
+};
+window.addEventListener('load', initializeSelectOptions());
+
+//Getting selected options HTML collection
+const getSelectedOptionsOnArrowButtonClick = (event) => {
+  if (event.target.matches('#left-arrow')) {
+    return rightSelect.selectedOptions;
+  } else if (event.target.matches('#right-arrow')) {
+    return leftSelect.selectedOptions;
+  }
+};
+
+//Removing selected elements from options
+const removeOptionsFromSelect = (arrayOfIndices) => {
+  for (let i = 0; i < arrayOfIndices.length; i++) {
+    let node = document.querySelector(`option[value="${arrayOfIndices[i]}"]`);
+    node.remove();
+  }
+};
+//Adding selected elements to options
+const addOptionsFromDataArrayToSelect = (selectNodeRef, arrayOfIndices) => {
+  let htmlOptionsArray = createSelectOptionsByIndex(arrayOfIndices);
+  console.log(htmlOptionsArray);
+  selectNodeRef.innerHTML += htmlOptionsArray.join('\n');
+};
+
+//Event listener and handler function for arrow clicks
+selectParent.addEventListener('click', (e) => handleArrowButtonClick(e));
+const handleArrowButtonClick = (e) => {
+  if (e.target.matches('#left-arrow') || e.target.matches('#right-arrow')) {
+    let selectedOptions = getSelectedOptionsOnArrowButtonClick(e);
+
+    let selectedIndices = [...selectedOptions].map((elem) => elem.value);
+    if (e.target.matches('#left-arrow')) {
+      removeOptionsFromSelect(selectedIndices);
+      addOptionsFromDataArrayToSelect(leftSelect, selectedIndices);
+    } else if (e.target.matches('#right-arrow')) {
+      removeOptionsFromSelect(selectedIndices);
+      addOptionsFromDataArrayToSelect(rightSelect, selectedIndices);
+    }
+  } else {
+    e.preventDefault();
+  }
+};
