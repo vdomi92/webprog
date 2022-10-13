f22db73c73a42d332ce50b221e257449382784e1
vataid <vatai.domonkos@szgyf.gov.hu>
2022-10-13 11:46:31 +0200

    fix

--- szorgalmi 2/multiselect-move/index.js
+++ szorgalmi 2/multiselect-move/index.js
@@ -1,8 +1,8 @@
-import { wordOptions } from './words.js';
+import { wordOptions } from "./words.js";
 
-const leftSelect = document.querySelector('#left-select');
-const rightSelect = document.querySelector('#right-select');
-const selectParent = document.querySelector('#multiselect-move');
+const leftSelect = document.querySelector("#left-select");
+const rightSelect = document.querySelector("#right-select");
+const selectParent = document.querySelector("#multiselect-move");
 
 //Function to generate select options from indices array and data array
 const createSelectOptionsByIndex = (indices) => {
@@ -16,15 +16,15 @@ const initializeSelectOptions = () => {
   let selectOptions = wordOptions.map((word, i) => {
     return `<option value="${i}"> ${word} </option>`;
   });
-  leftSelect.innerHTML = selectOptions.join('\n');
+  leftSelect.innerHTML = selectOptions.join("\n");
 };
-window.addEventListener('load', initializeSelectOptions());
+window.addEventListener("load", initializeSelectOptions());
 
 //Getting selected options HTML collection
 const getSelectedOptionsOnArrowButtonClick = (event) => {
-  if (event.target.matches('#left-arrow')) {
+  if (event.target.matches("#left-arrow")) {
     return rightSelect.selectedOptions;
-  } else if (event.target.matches('#right-arrow')) {
+  } else if (event.target.matches("#right-arrow")) {
     return leftSelect.selectedOptions;
   }
 };
@@ -35,22 +35,23 @@ const removeOptionsFromSelect = (arrayOfIndices) => {
     document.querySelector(`option[value="${arrayOfIndices[i]}"]`).remove();
   }
 };
+
 //Adding selected elements to options
 const addOptionsFromDataArrayToSelect = (selectNodeRef, arrayOfIndices) => {
   selectNodeRef.innerHTML +=
-    createSelectOptionsByIndex(arrayOfIndices).join('\n');
+    createSelectOptionsByIndex(arrayOfIndices).join("\n");
 };
 
 //Event listener and handler function for arrow clicks
-selectParent.addEventListener('click', (e) => handleArrowButtonClick(e));
+selectParent.addEventListener("click", (e) => handleArrowButtonClick(e));
 const handleArrowButtonClick = (e) => {
-  if (e.target.matches('#left-arrow') || e.target.matches('#right-arrow')) {
+  if (e.target.matches("#left-arrow") || e.target.matches("#right-arrow")) {
     let selectedOptions = getSelectedOptionsOnArrowButtonClick(e);
     let selectedIndices = [...selectedOptions].map((elem) => elem.value);
-    if (e.target.matches('#left-arrow')) {
+    if (e.target.matches("#left-arrow")) {
       removeOptionsFromSelect(selectedIndices);
       addOptionsFromDataArrayToSelect(leftSelect, selectedIndices);
-    } else if (e.target.matches('#right-arrow')) {
+    } else if (e.target.matches("#right-arrow")) {
       removeOptionsFromSelect(selectedIndices);
       addOptionsFromDataArrayToSelect(rightSelect, selectedIndices);
     }
